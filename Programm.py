65b6312abc6f0f2454c07629b84bc24eb9ecd8d0
SoHerrmann <88510936+SoHerrmann@users.noreply.github.com>
2021-08-13 17:34:56 +0200

    Update Programm.py
    Jetzt mit der Exception

--- Programm.py
+++ Programm.py
@@ -10,19 +10,15 @@ class Enzyme():
         self.as_sequence = as_sequence
         self.concentration = concentration
         self.unit = unit
-    def check_enzymes(self):
-        pass
+
 class Show_Enzyme(Enzyme):
     def __init__(self, name, as_sequence, concentration, unit):
         super().__init__(name, as_sequence, concentration, unit)
     def show(self):
         return self.name + ", " + self.as_sequence + ", " + self.concentration + ", " + self.unit
-test = Show_Enzyme("Aconitate Hydratase", "IHETHE", "0.1", "mol/l")
-print(test.show())
-
-
-# In[ ]:
-
-
-
-
+test = Show_Enzyme("Aconitate Hydratase", "IHETHE", 0.1, "mol/l")
+try:
+    print(test.show())
+except TypeError:
+    print("Falscher Datentyp")
+    

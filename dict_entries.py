ba145180613e4b7be910feec4397b2ee382426d8
Sofie Herrmann <sofie-abt@gmx.de>
2021-10-12 16:39:24 +0200

    dict_entries  + pdf

---
+++ dict_entries.py
@@ -0,0 +1,56 @@
+
+user_dict_entries = [
+                    {"key":"email",
+                    "label":"E-Mail"},
+                    {"key":"firstName",
+                    "label":"First Name"},
+                    {"key":"lastName",
+                    "label":"Last Name"},
+                    {"key":"institution",
+                    "label":"Institute"}
+                ]
+vessel_dict_entries = [
+                      {"key":"type",
+                      "label":"Vesseltype"},
+                      {"key":"unit",
+                      "label":"Unit"},
+                      {"key":"volume",
+                      "label":"Volume"}
+
+                ]
+
+condition_dict_entries = [
+                      {"key":"ph",
+                      "label":"pH"},
+                      {"key":"temp",
+                      "label":"Temperature"},
+                      {"key":"unit",
+                      "label":"Unit"}
+
+                ]
+
+buffer_dict_entries = [
+                      {"key":"concentration",
+                      "label":"Concentration"},
+                      {"key":"type",
+                      "label":"Buffertype"},
+                      {"key":"unit",
+                      "label":"Unit"}
+
+                ]
+
+enzymes_dict_entries = [
+                      {"key":"concentration",
+                      "label":"Concentration"},
+                      {"key":"ecNumber",
+                      "label":"ecNumber"},
+                      {"key":"formulation",
+                      "label":"Formulation"},
+                      {"key":"method",
+                      "label":"Method"},
+                      {"key":"name",
+                      "label":"Name"},
+                      {"key":"organism",
+                      "label":"Organism"},
+
+                ]
